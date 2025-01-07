45535c17970e7871db26cdf5be4172a85a7422b4
Benoit Lafon <benoit.lafon92@gmail.com>
2025-01-07 18:59:09 +0900

    Add basic Phong-type's lighting

--- shader/fragment_shader_edges.glsl
+++ shader/fragment_shader_edges.glsl
@@ -1,16 +1,25 @@
 #version 330 core
 
-in vec3 vertexPos;
+in vec3 fragPos;
+in vec3 fragNormal;
+
 out vec4 FragColor;
-uniform float fogNearDist = -1.0;
-uniform float fogFarDist = 6.0;
+
+uniform vec3 lightDir = normalize(vec3(0.5, -0.5, 0.5));
+uniform vec3 lightColor = vec3(1.0, 1.0, 1.0);
+uniform vec3 objectColor = vec3(1.0, 0.1, 0.2);
+uniform vec3 ambientColor = vec3(0.8, 0.8, 0.8);
 
 void main()
 {
-    vec4 fogColor    = vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 objectColor = vec4(1.0, 0.5, 0.2, 1.0);
-    float dist       = length(vertexPos);
-    float fogFactor  = smoothstep(fogNearDist, fogFarDist, dist);
-    vec4 color       = mix(objectColor, fogColor, fogFactor);
-    FragColor        = color;
+    vec3 norm = normalize(fragNormal);
+
+    float diff = max(dot(norm, -lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 ambient = ambientColor * lightColor;
+
+    vec3 finalColor = (ambient + diffuse) * objectColor;
+
+    FragColor = vec4(finalColor, 1.0);
 }
--- shader/vertex_shader_edges.glsl
+++ shader/vertex_shader_edges.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos;
-out vec3 vertexPos;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+
+out vec3 fragPos;
+out vec3 fragNormal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -9,7 +12,8 @@ uniform mat4 projection;
 
 void main()
 {
-    vec4 viewPos = view * model * vec4(aPos, 1.0);
-    gl_Position = projection * viewPos;
-    vertexPos = viewPos.xyz;
+    gl_Position = projection * view * model * vec4(position, 1.0);
+
+    fragPos    = vec3(model * vec4(position, 1.0));
+    fragNormal = mat3(transpose(inverse(model))) * normal;
 }
